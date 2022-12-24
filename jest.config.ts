2afc011d50904a422437a56c6d6a23910fc82e2e
João Feitoza <joaofeitoza.13@gmail.com>
2022-12-24 00:35:39 -0300

    chore: jest and it's configuration

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+ roots: ['<rootDir>/src'],
+ collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+ collectCoverage: true,
+ coverageDirectory: "coverage",
+ coverageProvider: "v8",
+ testEnvironment: "node",
+ transform: { '.+\\.ts$': 'ts-jest' },
+};
