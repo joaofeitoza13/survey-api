6a0691250ab86c30819ad58b53036c34cad1faef
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-02 16:40:59 -0300

    test: ensure LogControlleDecorator returns the same result as the controlle

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -45,4 +45,23 @@ describe('LogControllerDecorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should call controller handle', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'any_name'
+      }
+    })
+  })
 })
