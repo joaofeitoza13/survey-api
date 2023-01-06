7c1dd1040961076558a5c3e57fbe0c764a354ddb
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-06 15:29:28 -0300

    refactor: move bcrypt adapter files to a folder

--- src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -1,6 +1,6 @@
 import * as bcrypt from 'bcrypt'
-import { Hasher } from '../../data/protocols/cryptography/hasher'
-import { HashComparer } from '../../data/protocols/cryptography/hash-comparer'
+import { Hasher } from '../../../data/protocols/cryptography/hasher'
+import { HashComparer } from '../../../data/protocols/cryptography/hash-comparer'
 
 export class BcryptAdapter implements Hasher, HashComparer {
   private readonly salt
--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { LogControllerDecorator } from '../../decorators/log'
 import { Controller } from '../../../presentation/protocols'
