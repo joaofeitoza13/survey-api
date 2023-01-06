af813595a3863c9f586d11d52949b7b98b8f6a75
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-06 20:50:38 -0300

    refactor: move EmailValidatorAdapter to the main layer

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { makeSignUpController } from '../factories/signup/signup-factory'
-import { routeAdapter } from '../adapters/express-routes-adapter'
+import { routeAdapter } from '../adapters/express/express-routes-adapter'
 
 export default (router: Router): void => {
   router.post('/signup', routeAdapter(makeSignUpController()))
