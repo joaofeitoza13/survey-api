6359bda7ced4dd7062c297b4acd0008318dafff3
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-10 13:47:47 -0300

    test: ensure SignUpController returns 500 authentication throws

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -116,4 +116,13 @@ describe('SignUp Controller', () => {
       email: 'valid_email@email.com', password: 'valid_password'
     })
   })
+
+  test('Should return 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = await makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(
+      new Promise((resolve, reject) => reject(new Error()))
+    )
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
 })
