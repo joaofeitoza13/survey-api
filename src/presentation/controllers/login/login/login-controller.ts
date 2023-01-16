151286500078e6109ad8fe3c6af487fed1451026
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-16 00:11:01 -0300

    refactor: remove console.logs

--- src/presentation/controllers/login/login/login-controller.ts
+++ src/presentation/controllers/login/login/login-controller.ts
@@ -20,7 +20,6 @@ export class LoginController implements Controller {
       }
       return ok({ accessToken })
     } catch (error) {
-      console.log(error)
       return serverError(error)
     }
   }
