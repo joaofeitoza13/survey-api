2172bf60cea332d74a194801b2986a41862b8b94
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-17 12:09:45 -0300

    feat: add middleware adapter with AuthMiddleware to AddSurveyController post route

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import { routeAdapter } from '../adapters/express-routes-adapter'
+import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 
 export default (router: Router): void => {
-  router.post('/surveys', routeAdapter(makeAddSurveyController()))
+  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  router.post('/surveys', adminAuth, routeAdapter(makeAddSurveyController()))
 }
