b85fb93a9ed82c68bd1822bb5e147b075e58e323
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-16 22:47:43 -0300

    test: ensure JwtAdapter throws if verify() throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -54,4 +54,13 @@ describe('JWT Adapter', () => {
     const value = await sut.decrypt('any_token')
     expect(value).toBe('any_value')
   })
+
+  test('Should throw if verify throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const value = sut.decrypt('any_id')
+    await expect(value).rejects.toThrow()
+  })
 })
