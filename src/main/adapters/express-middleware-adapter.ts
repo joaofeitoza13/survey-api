1838f66da914cc5520f59d0b58e0fb9589526a22
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-17 12:05:52 -0300

    feat: create middleware adapter

---
+++ src/main/adapters/express-middleware-adapter.ts
@@ -0,0 +1,19 @@
+import { Middleware, HttpRequest } from '../../presentation/protocols'
+import { NextFunction, Request, Response } from 'express'
+
+export const adaptMiddleware = (middlware: Middleware) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const httpRequest: HttpRequest = {
+      headers: req.headers
+    }
+    const httpResponse = await middlware.handle(httpRequest)
+    if (httpResponse.statusCode === 200) {
+      Object.assign(req, httpResponse.body)
+      next()
+    } else {
+      res.status(httpResponse.statusCode).json({
+        error: httpResponse.body.message
+      })
+    }
+  }
+}
