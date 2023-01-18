72da788bd577df05de02634dc2193b3210dc593b
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-18 17:56:16 -0300

    refactor: rename middleware adapter

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -1,7 +1,7 @@
 import { Middleware, HttpRequest } from '../../presentation/protocols'
 import { NextFunction, Request, Response } from 'express'
 
-export const adaptMiddleware = (middleware: Middleware) => {
+export const middlewareAdapter = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const httpRequest: HttpRequest = {
       headers: req.headers
