17a1c212780d51ba6e954596a694be0b30e1f21c
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-19 20:01:46 -0300

    feat: ensure SaveSurveyResultController calls LoadSurveyById with correct values

---
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -0,0 +1,10 @@
+import { LoadSurveyById, Controller, HttpRequest, HttpResponse } from './save-survey-result-controller-protocols'
+
+export class SaveSurveyResultController implements Controller {
+  constructor (private readonly loadSurveyById: LoadSurveyById) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+    return null
+  }
+}
