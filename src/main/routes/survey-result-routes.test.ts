5ac024c55c149b6fcc2ea078c556ea43dbae78e9
João Feitoza <joaofeitoza.13@gmail.com>
2023-01-23 16:22:15 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -76,5 +76,25 @@ describe('Survey Routes', () => {
         .get('/api/surveys/any_id/results')
         .expect(403)
     })
+
+    test('Should return 200 on save survey with valid accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const surveyId = await surveyCollection.insertOne({
+        question: 'question',
+        answers: [{
+          answer: 'answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'answer 2',
+          image: 'http://image-name2.com'
+        }],
+        date: new Date()
+      }).then(result => result.insertedId)
+      const id = surveyId.toString()
+      await request(app)
+        .get(`/api/surveys/${id}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
